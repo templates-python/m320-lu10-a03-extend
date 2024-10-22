1870599609c3c5b8ec2bb84820cf2ce24082d00b
Marcel Suter <marcel.suter@bzz.ch>
2024-10-22 09:40:33 +0200

    solution done

--- mobile.py
+++ mobile.py
@@ -1,2 +1,19 @@
-class Mobile:
-    pass
+from phone import Phone
+
+class Mobile(Phone):
+
+    def __init__(self):
+        pass
+
+    def handle_sms(self):
+        print('sms senden und empfangen')
+
+    def what_i_am(self):
+        return('an old mobile')
+
+#Test
+if __name__ == '__main__':
+    mobile = Mobile()
+    print(mobile.what_i_am())
+    mobile.calling()
+    mobile.handle_sms()
--- phone.py
+++ phone.py
@@ -1,2 +1,17 @@
 class Phone:
-    pass
+    def __init__(self):
+        pass
+
+    def calling(self):
+        print('anrufen')
+
+    def what_i_am(self):
+        return ('a simply phone')
+
+    # Test
+
+
+if __name__ == '__main__':
+    p = Phone()
+    print(p.what_i_am())
+    p.calling()
