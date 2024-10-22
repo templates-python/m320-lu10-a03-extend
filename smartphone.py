6abdfd1d6217532ea8f294999b849a1904eb6765
Marcel Suter <marcel.suter@bzz.ch>
2024-10-22 09:40:33 +0200

    solution done

--- smartphone.py
+++ smartphone.py
@@ -1,2 +1,20 @@
-class SmartPhone:
-    pass
+from mobile import Mobile
+
+class SmartPhone(Mobile):
+
+    def __init__(self):
+        pass
+
+    def use_internet(self):
+        print('das Internet benutzen')
+
+    def what_i_am(self):
+        return ('a modern smartphone')
+
+#Test
+if __name__ == '__main__':
+    s = SmartPhone()
+    print(s.what_i_am())
+    s.calling()
+    s.handle_sms()
+    s.use_internet()
